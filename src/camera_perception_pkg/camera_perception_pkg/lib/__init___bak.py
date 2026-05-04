8d669df8e7dd3ab586c7175815221febcfead2b1
iamsungbhin <osb8252@gmail.com>
2026-05-04 16:48:08 +0900

    [HPC] Restore perception/decision_making .pyc libs and add gitignore execeptions

--- src/camera_perception_pkg/camera_perception_pkg/lib/__init___bak.py
+++ src/camera_perception_pkg/camera_perception_pkg/lib/__init___bak.py
@@ -3,7 +3,9 @@ import types
 import marshal
 
 def get_path(file_name=None):
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
+    p = os.path.dirname(os.path.abspath(__file__)).split("/")
+    LIB_PATH = os.path.join("/", *p[1:4], "src", *p[5:6],*p[5:6], "lib", file_name)
+    return LIB_PATH
 
 def get_pyc(module_file):
     file_path = get_path(module_file)
--- src/decision_making_pkg/decision_making_pkg/lib/__init___bak.py
+++ src/decision_making_pkg/decision_making_pkg/lib/__init___bak.py
@@ -3,7 +3,9 @@ import types
 import marshal
 
 def get_path(file_name=None):
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
+    p = os.path.dirname(os.path.abspath(__file__)).split("/")
+    LIB_PATH = os.path.join("/", *p[1:4], "src", *p[5:6],*p[5:6], "lib", file_name)
+    return LIB_PATH
 
 def get_pyc(module_file):
     file_path = get_path(module_file)
@@ -14,4 +16,4 @@ def get_pyc(module_file):
     exec(code, module.__dict__)
     return module
 
-camera_perception_func_lib = get_pyc("camera_perception_func_lib.cpython-310.pyc")
+decision_making_func_lib = get_pyc("decision_making_func_lib.cpython-310.pyc")
